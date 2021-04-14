d5e2d5d2699070dd396437120db35332d525f85e
hliu88 <17788596+hliu88@users.noreply.github.com>
2021-04-14 00:58:14 -0400

    Memory game update

---
+++ Brain_games/Assets/memorygame.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class memorygame : MonoBehaviour
+{
+
+    //int iter = 0;
+    int turn = 1;
+    double delta = 0;
+    int[][] levels = new int[50][];
+    int level = 1;
+    int levelProbs = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < 50; i++)
+        {
+            int[] problem = new int[i];
+            for (int z = 0; z < i; z++)
+            {
+                problem[z] = (Random.Range(0, 9));
+            }
+            levels[i] = problem;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(delta);
+        delta += 0.05;
+        if (Input.GetMouseButtonDown(0) && turn == 1)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit)
+            {
+                if (hit.collider.CompareTag("Square"))
+                {
+                    Debug.Log("bingo!");
+                }
+            }
+        }
+    }
+}
